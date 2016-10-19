e4d7aa8b9d5c0d955addb8f48c4f5f505aec4c8a
Alex Francois and Brad Copenhaver <alex@>
2016-10-19 11:46:11 -0700

    add front end function to call calculation for measurement conversions

--- js/scripts.js
+++ js/scripts.js
@@ -17,5 +17,24 @@ var bushelToPeck = function(bushels) {
 //Interface logic
 
 $(document).ready(function() {
-  $
+  $("form#g-to-l").submit(function(event){
+    event.preventDefault();
+    var gallons = parseFloat($("#gallon").val());
+    $("#litersOut").text(gallonToLiter(gallons));
+  });
+  $("form#l-to-g").submit(function(event){
+    event.preventDefault();
+    var liters = parseFloat($("#liter").val());
+    $("#gallonsOut").text(literToGallon(liters));
+  });
+  $("form#p-to-b").submit(function(event){
+    event.preventDefault();
+    var pecks = parseFloat($("#peck").val());
+    $("#bushelsOut").text(peckToBushel(pecks));
+  });
+  $("form#b-to-p").submit(function(event){
+    event.preventDefault();
+    var bushels = parseFloat($("#bushel").val());
+    $("#pecksOut").text(bushelToPeck(bushels));
+  });
 });
